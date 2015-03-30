14e46e83f556d5ddd6b8346a3b78a7c58c69b4f4
messyidea <the.mid.sea@gmail.com>
2015-03-30 11:17:49 +0800

    to 23.4

--- os/console.c
+++ os/console.c
@@ -350,7 +350,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
 	struct SHTCTL *shtctl = (struct SHTCTL *) *((int *) 0x0fe4);    //harimain中已经保存了全局变量的地址
 	struct SHEET *sht;
-	int *reg = &eax + 1;	/* eax后米娜的质地 */
+	int *reg = &eax + 1;	/* 用于api的返回值 */
 	//int cs_base = *((int *) 0xfe8);
     
     //不同的edx对应不同的api
@@ -372,13 +372,42 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		sheet_updown(sht, 3);	//背景放置在3的位置
 		reg[7] = (int) sht;
     } else if (edx == 6) {
-		sht = (struct SHEET *) ebx;
+		//sht = (struct SHEET *) ebx;
+        sht = (struct SHEET *) (ebx & 0xfffffffe);
 		putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *) ebp + ds_base);
-		sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+        if ((ebx & 1) == 0) {
+			sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+		}
+		//sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
 	} else if (edx == 7) {
-		sht = (struct SHEET *) ebx;
+		//sht = (struct SHEET *) ebx;
+        sht = (struct SHEET *) (ebx & 0xfffffffe);
 		boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
-		sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+        if ((ebx & 1) == 0) {
+			sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		}
+		//sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+	}  else if (edx == 8) {
+		memman_init((struct MEMMAN *) (ebx + ds_base));
+		ecx &= 0xfffffff0;	/* 以16字节为单位 */
+		memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
+	} else if (edx == 9) {
+		ecx = (ecx + 0x0f) & 0xfffffff0; /* 16字节为单位取整 */
+		reg[7] = memman_alloc((struct MEMMAN *) (ebx + ds_base), ecx);
+	} else if (edx == 10) {
+		ecx = (ecx + 0x0f) & 0xfffffff0; /* 16字节为单位取整 */
+		memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
+	} else if (edx == 11) {
+		//sht = (struct SHEET *) ebx;
+        sht = (struct SHEET *) (ebx & 0xfffffffe);
+		sht->buf[sht->bxsize * edi + esi] = eax;
+        if ((ebx & 1) == 0) {
+			sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+		} 
+    }else if (edx == 12) {
+		sht = (struct SHEET *) ebx;
+		sheet_refresh(sht, eax, ecx, esi, edi);
+		//sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
 	}
 	return 0;
 }
